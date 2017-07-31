fccae48fa00b9e2b31b128a46b7ba0fbb866be6d
Pranav Jain <pranajain@gmail.com>
2017-07-31 00:35:16 -0700

    Added function to post new jobs to Dirty Dog

--- server.js
+++ server.js
@@ -160,6 +160,48 @@ function callSendAPI(messageData) {
 }
 
 
+// ----------------------------------------------------------------------------
+// Dirty Dog Hauling specific code
+
+// See Postman to understand how it works
+// https://developers.facebook.com/docs/messenger-platform/send-api-reference
+
+function sendJobToDirtyDog(data) {
+	var options = { method: 'POST',
+	  url: 'http://dirtydoghauling.com/pawtracker/process/add_job.php',
+	  headers: 
+	   { 
+		 'content-type': 'application/x-www-form-urlencoded' },
+	  form: 
+	   { fran_id: '2', //constant
+		 jTime: '1501855200', //date converted to unix timecode
+		 trkID: '7', //constant, may change to have for specifically text based submissions
+		 jAddedBy: '69', // our user id (pranav jain)
+		 cFirstName: 'test', // collected by name
+		 cLastName: 'test', // find a way to split this up
+		 cAddress: '123 Test St. Middletown, PA 17112',
+		 cCity: 'Middletown', //you could pull this from geocoder...
+		 cState: 'PA', //you could pull this from geocoder...
+		 cZip: '17112', //you could pull this from geocoder...
+		 cHomeArea: '510', // phone number broken up into parts
+		 cHomePre: '579', // phone number broken up into parts
+		 cHomeSuf: '9664', // phone number broken up into parts
+		 jAddress: '123 Test St. Middletown, PA 17112', //always same as cAddress
+		 jCity: 'Middletown', //umm... you could pull this from geocoder...
+		 jState: 'PA', //umm... you could pull this from geocoder...
+		 jZip: '17112', //umm... you could pull this from geocoder...
+		 jJunkOther: 'Minimum Capable' // items would go here
+		 } };
+
+	request(options, function (error, response, body) {
+	  if (error) throw new Error(error);
+
+	  console.log(body);
+	});
+	
+}
+
+
 
 
 

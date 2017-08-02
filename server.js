80df4b0b4aa9f07ed722ca9d4ad7bd0d0652f4dc
Pranav Jain <pranajain@gmail.com>
2017-08-02 14:06:53 -0700

    fixing timestamp

--- server.js
+++ server.js
@@ -180,7 +180,9 @@ function sendJobToDirtyDog(order) {
 	const fname = order.name.split(' ').slice(0, -1).join(' ');
 	const lname = order.name.split(' ').slice(-1).join(' ');
 	const phone = order.phone.substring(2);
-	var d = new Date(order.time.substring(0, (order.time.length-6)));
+	//var d = new Date(order.time.substring(0, (order.time.length-6)));
+	var d = new Date(order.time);
+	console.log("Original getTime: " + d.getTime());
 	var timeStamp = d.getTime()/1000;
 	console.log("Date Object: " + d);
 	console.log("Timestamp: " + timeStamp);
